ecab4f076d56f7eb80b7efaa45a0743cb0978699
Felix Pasillas <pasillas.felix@yahoo.com>
2026-07-30 15:54:13 -0700

    Add authorization headers to API requests

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,10 @@
 const baseUrl = "https://se-flashcards-api.en.tripleten-services.com/v1";
 
+const headers = {
+  "Content-Type": "application/json",
+  Authorization: "019fb51d-58ce-7474-8f68-3f7b44bda743",
+};
+
 function processResponse(res) {
   if (res.ok) {
     return res.json();
@@ -8,7 +13,7 @@ function processResponse(res) {
 }
 
 function getDecks() {
-  return fetch(`${baseUrl}/decks`).then(processResponse);
+  return fetch(`${baseUrl}/decks`, { headers }).then(processResponse);
 }
 
 export { getDecks };
